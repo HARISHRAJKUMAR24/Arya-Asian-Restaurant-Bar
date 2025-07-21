05fa04cdc6e8bf9b8d713aa397ae2a0d266eeb9b
HARISHRAJKUMAR24 <harishpr546@gmail.com>
2025-07-21 20:36:32 +0530

    menu Finish

--- js/script.js
+++ js/script.js
@@ -15,3 +15,22 @@ function toggleOverlay() {
     const overlay = document.getElementById("fullscreenNav");
     overlay.classList.toggle("active");
 }
+
+
+// Show/hide "Go to Top" button
+window.addEventListener("scroll", function () {
+    const btn = document.getElementById("goTopBtn");
+    if (window.scrollY > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+});
+
+// Scroll to top function
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+
+
