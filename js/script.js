ad78d954ebe0a834fa080f04a1c2e20fdfabc727
HARISHRAJKUMAR24 <harishpr546@gmail.com>
2025-07-25 11:53:09 +0530

    scrool menu

--- js/script.js
+++ js/script.js
@@ -4,17 +4,7 @@ function toggleOverlay() {
     overlay.classList.toggle("active");
 }
 
-function scrollToSection(id) {
-    const element = document.getElementById(id);
-    if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-    }
-}
 
-function toggleOverlay() {
-    const overlay = document.getElementById("fullscreenNav");
-    overlay.classList.toggle("active");
-}
 
 
 // Show/hide "Go to Top" button
@@ -114,5 +104,16 @@ document.addEventListener('keydown', (e) => {
 
 
 
-//about page
+//menu scrooling 
 
+function scrollToSection(id) {
+    const element = document.getElementById(id);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+function toggleOverlay() {
+    const overlay = document.getElementById("fullscreenNav");
+    overlay.classList.toggle("active");
+}
